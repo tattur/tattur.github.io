1e8fde82eae09012ed0b9164eb0b2bf50abbfd95
tattur <74824140+tattur@users.noreply.github.com>
2020-12-12 18:24:11 +0900

    Create index_count.js

---
+++ index_count.js
@@ -0,0 +1,18 @@
+/*アクセス数を調べる。まずファイルを使えるようにする。*/
+var fs = WScript.CreateObject("Scripting.FileSystemObject");
+/*onLoadが発動した時に起動する関数*/
+function get_session(m){
+/*ファイルを読み込む*/
+var file_r = fs.OpenTextFile("count.txt",1);
+var str_count_r = file_r.ReadLine();
+file_r.Close();
+/*ファイルの文字を数値化し、1増やす*/
+m=Number(str_count);
+m=m+1;
+/*ファイルに書き込む*/
+var file_w = fs.OpenTextFile("count.txt",2);
+file_w.Write(String(m));
+file_w.Close();
+}
+/*これで数を保存することが可能*/
+onLoad=get_session
