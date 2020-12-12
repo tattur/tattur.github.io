ccd1f6e626ca7bb8fb19eb4b31826ac02d13d965
tattur <74824140+tattur@users.noreply.github.com>
2020-12-12 18:53:54 +0900

    Update index_count.js

--- index_count.js
+++ index_count.js
@@ -1,13 +1,13 @@
 /*アクセス数を調べる。まずファイルを使えるようにする。*/
 var fs = WScript.CreateObject("Scripting.FileSystemObject");
-/*onLoadが発動した時に起動する関数*/
-function get_session(m){
 /*ファイルを読み込む*/
 var file_r = fs.OpenTextFile("tattur.github.io/count.txt",1);
 var str_count_r = file_r.ReadLine();
 file_r.Close();
 /*ファイルの文字を数値化し、1増やす*/
 m=Number(str_count);
+/*onLoadが発動した時に起動する関数*/
+function get_session(m){
 m=m+1;
 /*ファイルに書き込む*/
 var file_w = fs.OpenTextFile("tattur.github.io/count.txt",2);
